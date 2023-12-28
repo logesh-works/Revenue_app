e51c8baca37734db16cac758c09f7642aedd2290
Logesh Kumar <logeshkumar.dev@gmail.com>
2023-12-28 16:03:56 +0530

    @logesh-works

--- csc_app/settings.py
+++ csc_app/settings.py
@@ -88,9 +88,13 @@ WSGI_APPLICATION = "csc_app.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cscdb',
+        'USER': 'root',
+        'PASSWORD': 'logi',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
