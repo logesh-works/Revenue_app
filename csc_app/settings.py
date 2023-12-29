2f19a598c245e423fab5edf9bb45b646ad9e4d25
Logesh Kumar <logeshkumar.dev@gmail.com>
2023-12-29 15:30:51 +0530

    @logesh-works

--- csc_app/settings.py
+++ csc_app/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import pymysql
+
+pymysql.version_info(1,4,6,'final',0)
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
