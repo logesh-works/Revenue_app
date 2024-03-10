9a21f2c100a92114f9a16c0a5842b48ca2ae56c1
Logesh Kumar <logeshkumar.dev@gmail.com>
2024-03-10 16:07:41 +0530

    @logesh-works

--- csc_app/settings.py
+++ csc_app/settings.py
@@ -93,23 +93,23 @@ WSGI_APPLICATION = "csc_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-""" DATABASES = {
+DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'u335740649_cscvdm',
-		'USER': 'u335740649_csc',
-		'PASSWORD': 'Cscvdm@2023',
-		'HOST':'82.180.143.103',
+		'NAME': 'cscdb',
+		'USER': 'cscdb',
+		'PASSWORD': 'Csc@606001',
+		'HOST':'localhost',
 		'PORT':'3306',
 	}
 }
- """
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
